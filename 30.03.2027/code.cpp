00a007e437a29003664007e58d35953a0112d126
Vidhi2807 <vidhimandaliya81@gmail.com>
2026-03-30 12:20:15 +0530

    Addition

--- 30.03.2027/code.cpp
+++ 30.03.2027/code.cpp
@@ -1,6 +1,7 @@
 
 
-
+//Template  
+//It is the keyword which is used to reuse the peice of code for diff data types !
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -62,5 +63,95 @@ int main(){
     cout << palindrome(v1) << endl;
     cout << palindrome(v2) << endl;
     cout << palindrome(v3) << endl;
+    return 0;
+}
+
+
+// different usetype 
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+template<class T1, class T2>
+
+class Pair{
+    
+    public:
+      T1 first;
+      T2 second;
+      
+      Pair(T1 a, T2 b){
+          first = a;
+          second = b;
+      }
+      
+      void display(){
+          cout << "First -> " << first << endl;
+          cout << "Second-> " << second << endl;
+      }
+    
+};
+
+// void add(T1 a, T2 b)
+// {
+//     cout << a+b << endl;
+// }
+
+// bool palindrome(vector<T1>& a){
+//     int i = 0;
+//     int j = a.size()-1;
+    
+//     while(i<j){
+//         if(a[i] != a[j]){
+//             return false;
+//         }
+        
+//         ++i;
+//         --j;
+//     }
+//     return true;
+// }
+// void add4(double a, float b){
+//     cout << a+b << endl;
+// }
+
+// void add3(int a, float b){
+//     cout << a+b <<endl;
+// }
+
+// void add2(double a, double b){
+//     cout << a+b <<endl;
+// }
+
+// void add1(float a, float b){
+//     cout << a+b <<endl;
+// }
+
+// void add(int a, int b){
+//     cout << a+b <<endl;
+// }
+
+int main(){
+    
+    // add(10, 20);//int , int
+    // add(10, 67.8);// int, float
+    // add(45.687, 54.869);
+    // add('A', 'g');
+    // // add("samir", "Hii");
+    
+    // // Palindrome....
+    // vector<int>v1 = {1,2,1};
+    // vector<char>v2 = {'a','b','a'};
+    // vector<int>v3 = {1,2,1,3};
+    
+    // cout << palindrome(v1) << endl;
+    // cout << palindrome(v2) << endl;
+    // cout << palindrome(v3) << endl;
+    
+    // map<> m1({});
+    
+    Pair<string, double> p1("Samir", 76.896);
+    p1.display();
     return 0;
 }
